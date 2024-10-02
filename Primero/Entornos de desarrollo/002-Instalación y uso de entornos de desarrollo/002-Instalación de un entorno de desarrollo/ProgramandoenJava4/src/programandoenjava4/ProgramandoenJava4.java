a07c41decc7f8f52d3625cab4517c76bc22a29d8
jocarsa <info@josevicentecarratala.com>
2024-10-02 20:40:22 +0200

    1

---
+++ Primero/Entornos de desarrollo/002-Instalación y uso de entornos de desarrollo/002-Instalación de un entorno de desarrollo/ProgramandoenJava4/src/programandoenjava4/ProgramandoenJava4.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package programandoenjava4;
+
+/**
+ *
+ * @author josevicente
+ */
+public class ProgramandoenJava4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola desde Entornos de Desarrollo");
+    }
+    
+}
