f6a2ba14736e668829d05b6698df0d986bff8105
jocarsa <info@josevicentecarratala.com>
2024-10-15 19:02:02 +0200

    1

---
+++ Segundo/Acceso a datos/002-Manejo de conectores/004-Ejecución de sentencias de descripción de datos/033-averiguo claves foraneas/index.php
@@ -0,0 +1,12 @@
+<?php
+	ini_set('display_errors', 1);																								// Activo errores
+	ini_set('display_startup_errors', 1);																				// Activo errores de inicio
+	error_reporting(E_ALL);																											// 
+	
+	include "ConexionDB.php";																										// incluyo el archivo en el cual se encuentra la clase
+	
+	$conexion = new conexionDB();																								// Creo una nueva instancia de la clase
+	
+	echo $conexion->seleccionaTabla("pedidos");																						// Llamo a un metodo
+	
+?>
